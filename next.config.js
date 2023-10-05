301d64ffd324aafb7177fde7fef7c295b88348f1
Prashant Das <prashantd@mindfiresolutions.com>
2023-10-05 20:54:45 +0530

    Configured custom generating build folder in next.config.js & added serve dev dependency for serving content of build folder

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  distDir: "build" // Optional: Change the output directory `out` -> `build`
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
