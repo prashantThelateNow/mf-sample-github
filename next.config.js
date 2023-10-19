fb0f4cc40760528348e911347870e16d221d0583
prashantThelateNow <55628792+prashantThelateNow@users.noreply.github.com>
2023-10-19 12:22:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "build" // Optional: Change the output directory `out` -> `build`
+  // output: "export",
+  // distDir: "build" // Optional: Change the output directory `out` -> `build`
 };
 
 module.exports = nextConfig;
